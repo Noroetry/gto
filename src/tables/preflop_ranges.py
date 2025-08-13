ea7ed82c8e2669a86debb27b8c3fe68c99f1108f
Noro <51792880+Noroetry@users.noreply.github.com>
2025-08-13 20:21:27 +0200

    aasd

--- src/tables/preflop_ranges.py
+++ src/tables/preflop_ranges.py
@@ -3,34 +3,45 @@ preflop_ranges = {
         "99", "TT", "JJ", "QQ", "KK", "AA",
         "A2s", "A3s", "A4s", "A5s", "A6s", "A7s", "A8s", "A9s", "ATs", "AJs", "AQs", "AKs",
         "ATo", "AJo", "AQo", "AKo",
-        "KQo", "KJo", "K7s", "K8s", "K9s", "KTs", "KJs", "KQs", "K5s",
+        "K5s", "K7s", "K8s", "K9s", "KTs", "KJs", "KQs",
+        "KJo", "KQo", 
         "QJs", "QTs"
     },
     "MP": {
         "77", "88", "99", "TT", "JJ", "QQ", "KK", "AA",
         "A2s", "A3s", "A4s", "A5s", "A6s", "A7s", "A8s", "A9s", "ATs", "AJs", "AQs", "AKs",
-        "A9o", "ATo", "AJo", "AQo", "AKo", "A5o",
-        "KTo", "KJo", "KQo", "K4s", "K5s", "K6s", "K7s", "K8s", "K9s", "KTs", "KJs", "KQs",
+        "A5o", "A9o", "ATo", "AJo", "AQo", "AKo",
+        "K4s", "K5s", "K6s", "K7s", "K8s", "K9s", "KTs", "KJs", "KQs",
+        "KTo", "KJo", "KQo",
         "Q9s", "QTs", "QJs",
         "JTs"
     },
     "CO": {
         "66", "77", "88", "99", "TT", "JJ", "QQ", "KK", "AA",
         "A2s", "A3s", "A4s", "A5s", "A6s", "A7s", "A8s", "A9s", "ATs", "AJs", "AQs", "AKs",
-        "A8o", "A9o", "ATo", "AJo", "AQo", "AKo", "A5o",
-        "KTo", "KJo", "KQo", "K3s", "K4s", "K5s", "K6s", "K7s", "K8s", "K9s", "KTs", "KJs", "KQs",
-        "Q9s", "QTs", "QJs", "J9s", "JTs", "QTo", "JTo"
+        "A5o", "A8o", "A9o", "ATo", "AJo", "AQo", "AKo",
+        "K3s", "K4s", "K5s", "K6s", "K7s", "K8s", "K9s", "KTs", "KJs", "KQs",
+        "KTo", "KJo", "KQo", 
+        "Q9s", "QTs", "QJs", 
+        "QTo", 
+        "J9s", "JTs",
+        "JTo",
+        "T9o"
     },
     "BTN": {
-        "22", "33", "44", "55", "66", "77", "88", "99", "TT", "JJ", "QQ", "KK", "AA",
+        "44", "55", "66", "77", "88", "99", "TT", "JJ", "QQ", "KK", "AA",
         "A2s", "A3s", "A4s", "A5s", "A6s", "A7s", "A8s", "A9s", "ATs", "AJs", "AQs", "AKs",
         "A3o", "A4o", "A5o", "A6o", "A7o", "A8o", "A9o", "ATo", "AJo", "AQo", "AKo",
         "K9o", "KTo", "KJo", "KQo",
         "K2s", "K3s", "K4s", "K5s", "K6s", "K7s", "K8s", "K9s", "KTs", "KJs", "KQs",
-        "Q5s", "Q6s", "Q7s", "Q8s", "Q9s", "QTs", "QJs",
-        "J7s", "J8s", "J9s", "JTs",
+        "Q5s", "Q6s", "Q7s", "Q8s", "Q9s", "QTs", "QJs", 
+        "QTo", "Q9o",
+        "J5s", "J7s", "J8s", "J9s", "JTs", 
+        "JTo", "J9o",
         "T7s", "T8s", "T9s",
-        "98s", "87s"
+        "T9o",
+        "98s",
+        "87s"
     },
     "SB": {
         "22", "33", "44", "55", "66", "77", "88", "99", "TT", "JJ", "QQ", "KK", "AA",
@@ -38,11 +49,16 @@ preflop_ranges = {
         "A3o", "A4o", "A5o", "A6o", "A7o", "A8o", "A9o", "ATo", "AJo", "AQo", "AKo",
         "K8o", "K9o", "KTo", "KJo", "KQo",
         "K2s", "K3s", "K4s", "K5s", "K6s", "K7s", "K8s", "K9s", "KTs", "KJs", "KQs",
-        "Q2s", "Q3s", "Q4s", "Q5s", "Q6s", "Q7s", "Q8s", "Q9s", "QTs", "QJs",
-        "J5s", "J6s", "J7s", "J8s", "J9s", "JTs",
+        "Q2s", "Q3s", "Q4s", "Q5s", "Q6s", "Q7s", "Q8s", "Q9s", "QTs", "QJs", 
+        "QTo", "Q9o",
+        "J5s", "J6s", "J7s", "J8s", "J9s", "JTs", "JTo", 
+        "JTo", "J9o",
         "T6s", "T7s", "T8s", "T9s",
-        "Q9o", "QTo", "J9o", "T9o",
-        "96s", "97s", "98s", "85s", "86s", "87s", "75s", "76s", "65s", "54s"
+        "96s", "97s", "98s", 
+        "85s", "86s", "87s", 
+        "75s", "76s", 
+        "65s", 
+        "54s"
     },
     "BB": set()
 }
